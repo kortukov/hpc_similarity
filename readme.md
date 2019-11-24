6970a3a02a6379ece10d22a2ff8409e422a254e1
eekortukov <eekortukov@yandex-team.ru>
2019-11-24 10:45:17 +0300

    Add readme and first py file

---
+++ readme.md
@@ -0,0 +1,3 @@
+A tool to find similar HPC application using system monitoring time-series data.
+
+Uses generalized feature structure feature extractor.
---
+++ src/feature_extractor/structures.py
@@ -0,0 +1,7 @@
+
+
+class ConstantStructure():
+    def __init__(parameters, t_series):
+        self.parameters = parameters
+        self.t_series = t_series
+
