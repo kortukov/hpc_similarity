20e7767e8f2703fcaa9aea547e62b091a34bbdea
eekortukov <eekortukov@yandex-team.ru>
2020-02-08 18:06:33 +0300

    Change preprocessing slightly to match stat2 better

--- src/feature_extractor/preprocessing.py
+++ src/feature_extractor/preprocessing.py
@@ -4,14 +4,19 @@ import scipy.signal
 
 
 def divide_by_max(time_series):
-    y_max = time_series.y.max()
+    y_max = np.max(time_series.y)
     if y_max > 0.001:
         time_series.y = time_series.y / y_max
     return time_series
 
 
-def median_filtration(time_series):
-    time_series['y'] = scipy.signal.medfilt(time_series['y'], kernel_size=5)
+def spike_removal(time_series, range_points=2):
+    # time_series['y'] = scipy.signal.medfilt(time_series['y'], kernel_size=5)
+    signal = time_series.y
+    n = len(signal)
+    for i in range(range_points, n - range_points - 1):
+        signal[i] = np.median(signal[i - range_points : i + range_points + 1])
+    time_series.y = signal
     return time_series
 
 
@@ -33,9 +38,19 @@ def find_timestamp_intersection(dataframes_dict):
 def get_time_delta(dataframes_dict):
     first_df = next(iter(dataframes_dict.values()))
     if len(first_df.time) < 3:
-        raise ValueError('Length of dataframe time is less than 3')
+        raise ValueError('Length of dataframe time is less than 3: {}'.format(dataframes_dict))
 
     return int((first_df.time[2] - first_df.time[1]) / 60)
+
+
+def check_length(dataframes_dict):
+    first_len = len(next(iter(dataframes_dict.values())))
+    if any(len(sensor_df) != first_len for sensor_df in dataframes_dict.values()):
+        raise ValueError(
+            'Incorrect lengths: {}'.format([len(df) for df in dataframes_dict.values()])
+        )
+    if any(len(sensor_df) == 0 for sensor_df in dataframes_dict.values()):
+        raise ValueError('Null lengths: {}'.format([len(df) for df in dataframes_dict.values()]))
 
 
 def preprocess_raw_sensor_data(raw_sensor_dataframes_dict, left_bound=None, right_bound=None):
@@ -60,18 +75,26 @@ def preprocess_raw_sensor_data(raw_sensor_dataframes_dict, left_bound=None, righ
 
     time_series_dict = {}
 
-    raw_sensor_dataframes_dict = find_timestamp_intersection(raw_sensor_dataframes_dict)
     time_delta = get_time_delta(raw_sensor_dataframes_dict)
+    raw_sensor_dataframes_dict = find_timestamp_intersection(raw_sensor_dataframes_dict)
+    check_length(raw_sensor_dataframes_dict)
     for sensor_name, raw_sensor_df in raw_sensor_dataframes_dict.items():
+        # Rename
         sensor_time_series = raw_sensor_df.loc[left_bound:right_bound, ['time', 'avg']]
         sensor_time_series = sensor_time_series.rename(columns={'time': 't', 'avg': 'y'})
-        sensor_time_series = divide_by_max(sensor_time_series)
-        sensor_time_series = median_filtration(sensor_time_series)
-        sensor_time_series.name = sensor_name
-        sensor_time_series.index = np.arange(1, len(sensor_time_series) + 1)
+
+        # Reduce size if too large
         short_y, short_t, times, _ = reduceSizeOfData(sensor_time_series.y, sensor_time_series.t)
         sensor_time_series.y, sensor_time_series.t = short_y, short_t
         time_delta *= times
+
+        # Normalize and remove spikes
+        sensor_time_series = divide_by_max(sensor_time_series)
+        sensor_time_series = spike_removal(sensor_time_series, 2)
+
+        sensor_time_series.name = sensor_name
+        sensor_time_series.index = np.arange(1, len(sensor_time_series) + 1)
+
         time_series_dict[sensor_name] = sensor_time_series
     return time_series_dict, time_delta
 
