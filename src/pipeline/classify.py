f9540712cab1997dfdfc872e19c354b83335c30c
eekortukov <eekortukov@yandex-team.ru>
2020-02-14 14:51:52 +0300

    Update classify script

--- src/pipeline/classify.py
+++ src/pipeline/classify.py
@@ -4,13 +4,14 @@ from os import listdir
 from os.path import isfile, join
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import textdistance
 
 import config.config as config
 
 
 def load_all_jobs():
-    all_jobs_directory_path = '/Users/eekortukov/Parallel/hpc_similarity/data/string_extracted/'
+    all_jobs_directory_path = '/Users/eekortukov/Parallel/hpc_similarity/data/extracted/'
     all_jobs_paths = [
         f for f in listdir(all_jobs_directory_path) if isfile(join(all_jobs_directory_path, f))
     ]
@@ -18,25 +19,28 @@ def load_all_jobs():
     for file_path in all_jobs_paths:
         job_id = file_path.rstrip('.pickle').split('job')[-1]
         with open(join(all_jobs_directory_path, file_path), 'rb') as file:
-            job_dict = pickle.load(file)
-            all_jobs[job_id] = job_dict
+            extracted_job_data = pickle.load(file)
+
+        # This is where string encoding happens
+        job_dict = {}
+        for sensor, superstructure in extracted_job_data['superstructure_dict'].items():
+            job_dict[sensor] = superstructure.get_string()
+        all_jobs[job_id] = job_dict
 
     return all_jobs
 
 
 def load_train_data():
     train_data_path = '/Users/eekortukov/Parallel/hpc_similarity/data/train_data/train.json'
-    # with open(train_data_path, 'rb') as file:
-    # train_data = json.load(file)
-    train_data = {'1077274:1077720': False, '1077274:1078502': False, '1077720:1078502': True}
+    with open(train_data_path, 'rb') as file:
+        train_data = json.load(file)
     return train_data
 
 
 def load_test_data():
     test_data_path = '/Users/eekortukov/Parallel/hpc_similarity/data/train_data/test.json'
-    # with open(test_data_path, 'rb') as file:
-    # test_data = json.load(file)
-    test_data = {'1077274:1077720': False, '1077274:1078502': False, '1077720:1078502': True}
+    with open(test_data_path, 'rb') as file:
+        test_data = json.load(file)
     return test_data
 
 
@@ -49,6 +53,7 @@ def calculate_distances(job1, job2):
         string1, string2 = job1[sensor], job2[sensor]
         # Here we can try various string distance methods to see which works better
         distance = textdistance.jaro_winkler(string1, string2)
+        # distance = textdistance.levenshtein(string1, string2)
         distances.append(distance)
 
     return distances
@@ -71,17 +76,53 @@ def train_and_estimate_accuracy(all_jobs, train_data, test_data=None):
 
     classifier = RandomForestClassifier(n_estimators=10)
     classifier.fit(train_distance_matrix, train_labels)
-    print('Train accuracy: {}'.format(classifier.score(train_distance_matrix, train_labels)))
+    print(
+        'Random forest train accuracy: {}'.format(
+            classifier.score(train_distance_matrix, train_labels)
+        )
+    )
 
     if test_data:
         test_pairs = [job_ids.split(':') for job_ids, label in test_data.items()]
         test_labels = [label for job_ids, label in test_data.items()]
         test_distance_matrix = get_distance_matrix(all_jobs, test_pairs)
-        print('Test accuracy: {}'.format(classifier.score(test_distance_matrix, test_labels)))
+        print(
+            'Random forest test accuracy: {}'.format(
+                classifier.score(test_distance_matrix, test_labels)
+            )
+        )
 
-    model_path = '/Users/eekortukov/Parallel/hpc_similarity/src/pipeline/model.pickle'
+    model_path = '/Users/eekortukov/Parallel/hpc_similarity/src/pipeline/rf_model.pickle'
     with open(model_path, 'wb+') as file:
         pickle.dump(classifier, file)
+
+    log_classifier = LogisticRegression(random_state=0, solver='liblinear')
+    log_classifier.fit(train_distance_matrix, train_labels)
+    print(
+        'Logistic regression train accuracy: {}'.format(
+            log_classifier.score(train_distance_matrix, train_labels)
+        )
+    )
+
+    if test_data:
+        test_pairs = [job_ids.split(':') for job_ids, label in test_data.items()]
+        test_labels = [label for job_ids, label in test_data.items()]
+        test_distance_matrix = get_distance_matrix(all_jobs, test_pairs)
+        print(
+            'Logistic regression test accuracy: {}'.format(
+                log_classifier.score(test_distance_matrix, test_labels)
+            )
+        )
+
+    log_reg_coefs = {
+        sensor: coef for coef, sensor in zip(log_classifier.coef_[0], config.SENSORS_LIST)
+    }
+    print('Logistic regression coefs: {}')
+    pprint.pprint(log_reg_coefs)
+
+    model_path = '/Users/eekortukov/Parallel/hpc_similarity/src/pipeline/lr_model.pickle'
+    with open(model_path, 'wb+') as file:
+        pickle.dump(log_classifier, file)
 
 
 @command()
